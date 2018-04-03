7e7760d791385e04bcdabc22fd1b1aae1fa7cbef
William Park <wjypark94@gmail.com>
2018-04-03 01:23:48 -0700

    added more dependencies with mock data

--- server.js
+++ server.js
@@ -2,9 +2,13 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 
+mongoose.Promise = global.Promise;
 const app = express();
 
+app.use(morgan('common'));
 app.use(express.static('public'));
 
 if (require.main === module) {
