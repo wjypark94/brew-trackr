4eb92f3bbd8e87d454d3595f1c3613cfe54a5041
William Park <wjypark94@gmail.com>
2018-04-19 20:04:12 -0700

    fixed typo

--- config.js
+++ config.js
@@ -13,7 +13,7 @@
 
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/brewtrackr';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/brewtrackr';
+
 
 exports.PORT = process.env.PORT || 8080
 
